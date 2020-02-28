9b3ca2853b9c9f1d136f7c84b13911a6b81bc733
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-28 21:22:23 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+attr_accessor :name, :artist, :genre
+@@count = 0
+@@artists =  []
+@@genres = []
+
+def initialize(name, artist, genre)
+  @name = name
+  @artist = artist
+  @genre = genre
+  @@artists << artist
+  @@genres << genre
+  @@count += 1
+end
+def self.count
+  @@count
+
+
+end
+end

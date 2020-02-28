d1f98733feef04b7e837b2554b207614ddc7159c
Cameron J Leverett <cameron.leverett@yahoo.com>
2020-02-28 22:28:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,8 @@ attr_accessor :name, :artist, :genre
 @@count = 0
 @@artists =  []
 @@genres = []
+@@genre_count = {}
+
 
 def initialize(name, artist, genre)
   @name = name
@@ -12,6 +14,21 @@ def initialize(name, artist, genre)
   @@genres << genre
   @@count += 1
 end
+def self.artists
+  @@artists.uniq!
+end
+def self.genres
+  @@genres.uniq!
+end
+def self.genre_count
+  @@genres.each do |genre| genre.to_s
+
+end
+end
+
+def artist_count
+end
+
 def self.count
   @@count
 

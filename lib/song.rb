6ad035ecb5b60db8862a95dbecfdc87c4861e3af
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-03-06 19:13:53 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+
+  def initialize(name, artist, genre)
+      @name = name
+      @artist = artist
+      @genre = genre
+      @@count = []
+  end
+
+
+
+
+
+
+
+
+
+end
